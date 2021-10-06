c7efa47ae14afe01ce477394344110fa46fee503
Konrad <konradformedia@gmail.com>
2021-10-06 10:21:12 +0200

    Test Commit

--- src/main/java/server/repositories/SongRepository.java
+++ src/main/java/server/repositories/SongRepository.java
@@ -1,6 +1,5 @@
 package server.repositories;
 
-
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import server.models.Song;
